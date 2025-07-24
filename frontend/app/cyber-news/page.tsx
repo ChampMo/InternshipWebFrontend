48adcb09fcbf01b6585e5f9f227f05dbb20ae7c8
Monthol <97272854+ChampMo@users.noreply.github.com>
2025-07-24 11:24:00 +0700

    .

--- frontend/app/cyber-news/page.tsx
+++ frontend/app/cyber-news/page.tsx
@@ -1,7 +1,26 @@
 'use client'
 
+<<<<<<< Updated upstream
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { Icon } from '@iconify/react'
 
 import { useToast } from '@/context/ToastContext'
+=======
+import React, { useEffect, useState } from 'react'
+import Sidebar from '@/src/components/sidebar'
+
+function CyberNews() {
+
+
+  return (
+    <div className='flex'>
+      <Sidebar pageName={'Cyber News'}/>
+      <h1>CyberNews</h1>
+      
+    </div>
+  )
+}
+
+export default CyberNews
+>>>>>>> Stashed changes
---
+++ frontend/src/components/sidebar.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import Link from 'next/link'
+import { useParams } from 'next/navigation';
+import '@/app/globals.css'; // Ensure global styles are applied
+
+interface SidebarProps {
+    pageName?: string;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({pageName}) => {
+    const menuItems = [
+        { name: 'Jira Dashboard', path: '/jira-dashboard' },
+        { name: 'Cyber News', path: '/cyber-news' },
+        { name: 'TI Tech Intelligence', path: '/ti-tech-intelligence' },
+    ];
+    const menuAdmin = [
+        { name: 'User Management', path: '/user-management' },
+        { name: 'Token Management', path: '/token-management' },
+        { name: 'Cyber News Management', path: '/cyber-news-management' },
+        { name: 'Settings', path: '/settings' },
+    ];
+
+    const params = useParams()
+
+
+    return (
+        <aside className='bg-miniblue w-80 h-screen shadow-lg rounded-r-3xl flex flex-col'>
+            <header className='p-4 h-40'>
+                <div className='text-xl bitcount-prop-single' >Cyber Command</div>
+            </header>
+            <div className='px-4 py-2'>
+                <div className='text-sm text-gray-400'>Product</div>
+            </div>
+            {/* Render menu items */}
+            <nav>
+                <div className='flex flex-col'>
+                    {menuItems.map((item, index) => (
+                        <React.Fragment key={item.path}>
+                            <Link
+                                href={item.path}
+                                className={`text-lg px-8 py-3 rounded-r-lg duration-200 ${pageName === item.name ? 'bg-primary1 text-white' : ' hover:bg-primary2'}`}>
+                                {item.name}
+                            </Link>
+                            {index !== menuItems.length - 1 && (
+                                <div className='border-b border-gray-200 w-10/12 mx-auto'></div>
+                            )}
+                        </React.Fragment>
+                    ))}
+                </div>
+            </nav>
+            <div className='px-4 py-2'>
+                <div className='text-sm text-gray-400 mt-4'>Admin</div>
+            </div>
+            {/* Render menu items */}
+            <nav>
+                <div className='flex flex-col'>
+                    {menuAdmin.map((item, index) => (
+                        <React.Fragment key={item.path}>
+                            <Link
+                            href={item.path}
+                            className={`text-lg px-8 py-3 rounded-r-lg duration-200 ${pageName === item.name ? 'bg-primary1 text-white' : ' hover:bg-primary2'}`}>
+                                {item.name}
+                            </Link>
+                            {index !== menuAdmin.length - 1 && (
+                                <div className='border-b border-gray-200 w-10/12 mx-auto'></div>
+                            )}
+                        </React.Fragment>
+                    ))}
+                </div>
+            </nav>
+        </aside>
+    );
+};
+
+export default Sidebar;
