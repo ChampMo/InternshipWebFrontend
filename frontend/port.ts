0d84c246cef6ec2aba936bc5569defc213c21fd5
Monthol <97272854+ChampMo@users.noreply.github.com>
2025-09-29 10:58:49 +0700

    update env

--- frontend/port.ts
+++ frontend/port.ts
@@ -1,7 +1,9 @@
+import { env } from "process";
+
 const Port = {
   //  BASE_URL: 'http://localhost:8000/api',
-  BASE_URL: 'https://internship-web-backend.vercel.app/api',
-  
+  BASE_URL: env.BASE_URL,
+
 };
 
 export default Port;
