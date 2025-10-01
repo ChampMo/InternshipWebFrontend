ccf413b70a85ca5addd05b76dc8cb240cd3a7092
Monthol <97272854+ChampMo@users.noreply.github.com>
2025-10-01 09:52:02 +0700

    Update port.ts

--- frontend/port.ts
+++ frontend/port.ts
@@ -2,7 +2,7 @@
 
 const Port = {
   //  BASE_URL: 'http://localhost:8000/api',
-  BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  BASE_URL: process.env.NEXT_PUBLIC_BASE_URL+'/api',
   // BASE_URL: 'https://internship-web-backend.vercel.app/api'
 
 };
